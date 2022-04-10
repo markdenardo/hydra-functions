e1ecde1c3e652584a1854d5c771942c047828142
MDN <mark.denardo@gmail.com>
2022-04-10 13:11:24 -0400

    Create variables.js

---
+++ variables.js
@@ -0,0 +1,11 @@
+const t=(()=>time)
+const f=(()=>a.fft[0])
+const pi=(()=>Math.random(Math.PI))
+
+function something(){s0.initScreen();
+src(s0).modulateScrollX(osc(10),0.5,0).rotate(()=>a.fft[0],0.1)
+.repeat(()=>time,1)
+.add(o0,0.1)
+.out(o0)}
+something()
+render(o0) 
